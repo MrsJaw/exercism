e86ebe8b84929bf9085880f9ae301778be7a31d9
Julia <MrsJaw@users.noreply.github.com>
2016-02-01 23:02:38 -0600

    initial commit add the leap solution

---
+++ Leap/Leap/Leap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Year = 0;
+            bool ValidYearInput = true;
+            do
+            {
+                Console.Write("Enter a year to test: ");
+                string UserInput = Console.ReadLine();
+                ValidYearInput = int.TryParse(UserInput, out Year);
+                Console.WriteLine();
+                if(ValidYearInput)
+                {
+                    Console.WriteLine("{0} {1} a leap year.", Year.ToString(), LeapSolution.Year.IsLeap(Year) ? "was" : "was not");
+                }
+            } while (ValidYearInput);
+        }
+    }
+}
---
+++ Leap/Leap/LeapSolution/Year.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapSolution
+{
+    public class Year
+    {
+        public static bool IsLeap(int year)
+        {
+            bool Result = false;
+
+            if (year % 400 == 0)
+            {
+                Result = true;
+            }
+            else if (year % 100 == 0)
+            {
+                Result = false;
+            }
+            else if (year % 4 == 0)
+            {
+                Result = true;
+            }
+
+            return Result;
+        }
+    }
+}
