5dcf4dc4685ad4ee81f3c63f3b7a534f87f0c3bf
Julia <MrsJaw@users.noreply.github.com>
2016-02-06 12:57:11 -0600

    Added Bob solution

---
+++ Bob/Bob/Bob/Bob.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bob
+{
+    public class Bob
+    {
+        public static string Hey(string userInput)
+        {
+            string Result = "Whatever.";
+            string FormattedUserInput = userInput.Trim();
+
+            if (!string.IsNullOrWhiteSpace(FormattedUserInput))
+            {
+                //Bob answers 'Sure.' if you ask him a question.
+                if (FormattedUserInput.EndsWith("?"))
+                {
+                    Result = "Sure.";
+                }
+                //He answers 'Whoa, chill out!' if you yell at him.
+                Regex FindCapitalAlphabetical = new Regex("[A-Z]+");
+                if (FormattedUserInput.Equals(FormattedUserInput.ToUpper()) && FindCapitalAlphabetical.IsMatch(FormattedUserInput))
+                {
+                    Result = "Whoa, chill out!";
+                }                
+            }
+            else
+            {
+                //He says 'Fine. Be that way!' if you address him without actually saying anything.
+                Result = "Fine. Be that way!";
+            }
+           
+            //He answers 'Whatever.' to anything else.
+            return Result;
+           
+        }
+    }
+}
---
+++ Bob/Bob/BobTester/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ValidYearInput = true;
+            do
+            {
+                Console.Write("Enter input to test: ");
+                string UserInput = Console.ReadLine();
+                Console.WriteLine();
+                if (ValidYearInput)
+                {
+                    Console.WriteLine("You: {0} Bob: {1}", UserInput, Bob.Bob.Hey(UserInput));
+                }
+            } while (ValidYearInput);
+        }
+    }
+}
